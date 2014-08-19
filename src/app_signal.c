03c8d32273335a3ffbaa998fe1114415e134f875
Janos Kovacs <jankovac503@gmail.com>
2014-08-19 20:08:56 +0300

    fix __app_dbus_signal_filter() not to fail on a valid unix user
    When the AUL clients received a DBus app_launch or app_dead signal
    the __app_dbus_signal_filter() function, that handles the message,
    failed for every user ID that is not 0, ie. for all valid user ID's.
    
    As a result the clients notification routines were not called and
    the applications could not be tracked.
    
    Fixes: TC-1514
    
    Change-Id: Ib67ddfaebf62d26f01256c625d13d770e71f8d48

--- src/app_signal.c
+++ src/app_signal.c
@@ -50,7 +50,7 @@ __app_dbus_signal_filter(DBusConnection *conn, DBusMessage *message,
 	if (sender == NULL)
 		return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
 
-	if (dbus_bus_get_unix_user(conn, sender, &error) != 0) {
+	if (dbus_bus_get_unix_user(conn, sender, &error) < 0) {
 		_E("reject by security issue - no allowed sender\n");
 		dbus_error_free(&error);
 		return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
