2a9644fdff3d3ffb29d13bc7352bcf288ec1791c
Sangyoon Jang <s89.jang@samsung.com>
2015-08-28 19:25:13 +0900

    Send result of APP_TERM_REQ_PID to caller
    Change-Id: Ib93418aa223301964c5bd1afbaff4440f8eb1e19
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

--- am_daemon/amd_request.c
+++ am_daemon/amd_request.c
@@ -199,9 +199,7 @@ static int __app_process_by_pid(int cmd,
 		__real_send(clifd, ret);
 		break;
 	case APP_TERM_REQ_BY_PID:
-		if ((ret = __app_send_raw(pid, APP_TERM_REQ_BY_PID, (unsigned char *)&dummy, sizeof(int))) < 0) {
-			_D("terminate req packet send error");
-		}
+		ret = _term_req_app(pid, clifd);
 		break;
 	case APP_TERM_BY_PID_ASYNC:
 		if ((ret = __app_send_raw_with_noreply(pid, cmd, (unsigned char *)&dummy, sizeof(int))) < 0) {
