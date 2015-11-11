03a7a4928afcd8d2d7814741d16a4097a3523e90
Daehyeon Jung <darrenh.jung@samsung.com>
2015-11-11 14:47:40 +0900

    Add widget status update APIs
    - aul_widget_update_status()
    - aul_widget_listen_status()
    
    Change-Id: Ia8cbd9b7b331c8cebeca52b48af38f5bac8185a4
    Signed-off-by: Daehyeon Jung <darrenh.jung@samsung.com>

--- src/status.c
+++ src/status.c
@@ -23,6 +23,7 @@
 #include <aul.h>
 #include <bundle.h>
 
+#include "simple_util.h"
 #include "aul_util.h"
 #include "app_sock.h"
 #include "aul_api.h"
@@ -37,6 +38,9 @@ typedef struct _app_status_cb_info_t {
 app_status_cb_info_t *app_status_cb = NULL;
 
 static int app_status = STATUS_LAUNCHING;
+
+static int (*_aul_widget_handler) (const char *widget_id, const char *instance_id, int status, bundle *extra, void *data) = NULL;
+static void *_aul_widget_data;
 
 SLPAPI int aul_status_update(int status)
 {
@@ -164,6 +168,82 @@ SLPAPI int aul_running_list_update(char *appid, char *app_path, char *pid)
 
 	if (kb != NULL)
 			bundle_free(kb);
+
+	return ret;
+}
+
+int app_widget_update(bundle *b)
+{
+	const char *widget_id;
+	const char *instance_id;
+	const char *status_str;
+	int status = 0;
+
+	if (b == NULL) {
+		_E("invalid arguments");
+		return -1;
+	}
+
+	widget_id = bundle_get_val(b, AUL_K_APPID);
+	instance_id = bundle_get_val(b, AUL_K_WIDGET_INSTANCE_ID);
+	status_str = bundle_get_val(b, AUL_K_WIDGET_INSTANCE_STATUS);
+	if (status_str)
+		status = atoi(status_str);
+
+	_D("widget_id: %s instnace_id: %s status: %d", widget_id, instance_id, status);
+
+	if (_aul_widget_handler) {
+		_aul_widget_handler(widget_id, instance_id, status, b, _aul_widget_data);
+	}
+
+	return 0;
+}
+
+SLPAPI int aul_listen_widget_status(int (*aul_handler)(const char *widget_id, const char *instance_id, int status, bundle *extra, void *data), void *user_data)
+{
+	int pid = getpid();
+	int ret = 0;
+
+	_aul_widget_handler = aul_handler;
+	_aul_widget_data = user_data;
+
+	ret = __app_send_raw(AUL_UTIL_PID, APP_LISTEN_WIDGET, (unsigned char *)&pid, sizeof(pid));
+
+	return ret;
+}
+
+SLPAPI int aul_update_widget_status(const char *widget_id, const char *instance_id, int status, bundle *data, int receiver_pid)
+{
+	bundle *b = NULL;
+	char status_buf[6];
+	char pid_buf[6];
+	int ret = 0;
+
+	if (!widget_id || !instance_id)
+		return AUL_R_EINVAL;
+
+	if (data)
+		b = bundle_dup(data);
+	else
+		b = bundle_create();
+
+	if (!b) {
+		_E("out of memory");
+		return -1;
+	}
+
+	bundle_add(b, AUL_K_APPID, widget_id);
+	bundle_add(b, AUL_K_WIDGET_INSTANCE_ID, instance_id);
+	sprintf(status_buf, "%d", status);
+	bundle_add(b, AUL_K_WIDGET_INSTANCE_STATUS, status_buf);
+	if (receiver_pid) {
+		sprintf(pid_buf, "%d", receiver_pid);
+		bundle_add(b, AUL_K_WIDGET_INTERNAL_STATUS, pid_buf);
+	}
+
+	ret = app_send_cmd_with_noreply(AUL_UTIL_PID, APP_WIDGET_UPDATE, b);
+
+	bundle_free(b);
 
 	return ret;
 }
