a5b618e5508c5af66d688951ec2be2a45d5facb4
Sangyoon Jang <s89.jang@samsung.com>
2016-02-04 10:21:30 +0900

    Implement resource parser
    resource parser is implemented at pkgmgr-info currently, but aul is the
    only project who uses resource parser.
    
    Change-Id: I5a98ac19cf06a07eab09ffce0b9ec4830b697495
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

---
+++ src/rsc_reader.c
@@ -0,0 +1,223 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <glib.h>
+#include <libxml/tree.h>
+
+#include <dlog.h>
+#include <bundle.h>
+
+#ifdef LOG_TAG
+#undef LOG_TAG
+#endif
+
+#define LOG_TAG "AUL_RESOURCE_MANAGER"
+
+/* TODO: move these to private header */
+#define RSC_NODE_ATTR_SCREEN_DPI "screen-dpi"
+#define RSC_NODE_ATTR_SCREEN_DPI_RANGE "screen-dpi-range"
+#define RSC_NODE_ATTR_SCREEN_WIDTH_RANGE "screen-width-range"
+#define RSC_NODE_ATTR_SCREEN_LARGE "screen-large"
+#define RSC_NODE_ATTR_SCREEN_BPP "screen-bpp"
+#define RSC_NODE_ATTR_PLATFORM_VER "platform-version"
+#define RSC_NODE_ATTR_LANGUAGE "language"
+
+typedef struct {
+	char *folder;
+	bundle *attr;
+} resource_node_t;
+
+typedef struct {
+	char *folder;
+	char *type;
+	GList *node_list;
+} resource_group_t;
+
+typedef struct {
+	char *package;
+	GList *group_list;
+} resource_data_t;
+
+/*
+struct node {
+	char *folder;
+	bundle *attr;
+};
+
+struct group {
+	char *folder;
+	char *type;
+	GList *node;
+};
+*/
+
+static char *_get_attribute(xmlNode *xml_node, const char *name)
+{
+	xmlChar *val;
+	char *attr = NULL;
+
+	val = xmlGetProp(xml_node, (const xmlChar *)name);
+	if (val) {
+		attr = strdup((const char *)val);
+		xmlFree(val);
+	}
+
+	return attr;
+}
+
+static void _get_attribute_into_bundle(xmlNode *xml_node, const char *name,
+		bundle *b)
+{
+	char *attr;
+
+	attr = _get_attribute(xml_node, name);
+	if (attr)
+		bundle_add_str(b, name, attr);
+}
+
+static int _parse_node(xmlNode *xml_node, GList **nodes)
+{
+	resource_node_t *node;
+
+	if (strcmp((char *)xml_node->name, "node"))
+		return -1;
+
+	node = calloc(1, sizeof(resource_node_t));
+	node->folder = _get_attribute(xml_node, "folder");
+	/* why we should use bundle here? */
+	node->attr = bundle_create();
+	_get_attribute_into_bundle(xml_node, RSC_NODE_ATTR_SCREEN_DPI,
+			node->attr);
+	_get_attribute_into_bundle(xml_node, RSC_NODE_ATTR_SCREEN_DPI_RANGE,
+			node->attr);
+	_get_attribute_into_bundle(xml_node, RSC_NODE_ATTR_SCREEN_WIDTH_RANGE,
+			node->attr);
+	_get_attribute_into_bundle(xml_node, RSC_NODE_ATTR_SCREEN_LARGE,
+			node->attr);
+	_get_attribute_into_bundle(xml_node, RSC_NODE_ATTR_SCREEN_BPP,
+			node->attr);
+	_get_attribute_into_bundle(xml_node, RSC_NODE_ATTR_PLATFORM_VER,
+			node->attr);
+	_get_attribute_into_bundle(xml_node, RSC_NODE_ATTR_LANGUAGE,
+			node->attr);
+
+	*nodes = g_list_append(*nodes, node);
+
+	return 0;
+}
+
+static char *_get_group_type(xmlNode *xml_node)
+{
+	static const char delim[] = "-";
+	char *str;
+	char *tok;
+	char *ptr;
+
+	/* copy original string */
+	str = strdup((const char *)xml_node->name);
+	tok = strtok_r(str, delim, &ptr);
+	/* not a group element */
+	if (tok == NULL || strcmp(tok, "group"))
+		return NULL;
+	tok = strtok_r(NULL, delim, &ptr);
+	/* invalid element */
+	if (tok == NULL)
+		return NULL;
+	ptr = strdup(tok);
+	free(str);
+
+	return ptr;
+}
+
+static int _parse_group(xmlNode *xml_node, GList **groups)
+{
+	xmlNode *tmp;
+	char *type;
+	resource_group_t *group;
+
+	type = _get_group_type(xml_node);
+	if (type == NULL)
+		return -1;
+
+	group = calloc(1, sizeof(resource_group_t));
+	group->type = type;
+	group->folder = _get_attribute(xml_node, "folder");
+
+	for (tmp = xml_node->children; tmp; tmp = tmp->next) {
+		if (xml_node->type != XML_ELEMENT_NODE)
+			continue;
+		if (_parse_node(tmp, &group->node_list))
+			continue;
+	}
+
+	*groups = g_list_append(*groups, group);
+
+	return 0;
+}
+
+static int _parse_resource(xmlNode *xml_node, resource_data_t **data)
+{
+	xmlNode *tmp;
+
+	*data = calloc(1, sizeof(resource_data_t));
+
+	for (tmp = xml_node->children; tmp; tmp = tmp->next) {
+		if (tmp->type != XML_ELEMENT_NODE)
+			continue;
+		_parse_group(tmp, &(*data)->group_list);
+	}
+
+	return 0;
+}
+
+int _resource_open(const char *path, resource_data_t **data)
+{
+	int ret;
+	xmlDoc *doc;
+	xmlNode *root;
+
+	/* TODO: validate ? */
+	doc = xmlReadFile(path, NULL, 0);
+	if (doc == NULL)
+		return -1;
+	root = xmlDocGetRootElement(doc);
+
+	ret = _parse_resource(root, data);
+
+	xmlFreeDoc(doc);
+
+	return ret;
+}
+
+static void __free_resource_node(gpointer data)
+{
+	resource_node_t *node = (resource_node_t *)data;
+
+	free(node->folder);
+	bundle_free(node->attr);
+	free(node);
+}
+
+static void __free_resource_group(gpointer data)
+{
+	resource_group_t *group = (resource_group_t *)data;
+
+	free(group->folder);
+	free(group->type);
+
+	g_list_free_full(group->node_list, __free_resource_node);
+
+	free(group);
+}
+
+int _resource_close(resource_data_t *data)
+{
+	/*
+	free(data->package);
+	*/
+	g_list_free_full(data->group_list, __free_resource_group);
+
+	free(data);
+
+	return 0;
+}
