e4cb840f96c851ce35d1572f4b9aef9bb682ce0d
Daehyeon Jung <darrenh.jung@samsung.com>
2016-07-04 15:06:10 +0900

    Add aul_widget_instance_* function
     - Add aul_widget_instance_* functions to store instance
     to AMD
    
    Change-Id: I3c657cc81181031d0ffd644e2d726c456b9fc0d8
    Signed-off-by: Daehyeon Jung <darrenh.jung@samsung.com>

---
+++ src/widget.c
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2000 - 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <glib.h>
+#include <bundle.h>
+#include <aul.h>
+#include <aul_util.h>
+#include <aul_sock.h>
+#include <aul_cmd.h>
+#include <aul_error.h>
+
+static const char *__to_appid(const char *widget_id)
+{
+	const char *appid;
+	appid = g_strstr_len(widget_id, strlen(widget_id), "@") + 1;
+	if (appid != (const char *)1) {
+		if (appid > widget_id + (sizeof(char) * strlen(widget_id)))
+			appid = (char *)widget_id;
+	} else {
+		appid = (char *)widget_id;
+	}
+
+	return appid;
+}
+
+API int aul_widget_instance_add(const char *widget_id, const char *instance_id)
+{
+	int ret;
+	bundle *kb;
+
+	if (widget_id == NULL || instance_id == NULL)
+		return AUL_R_EINVAL;
+
+	kb = bundle_create();
+	if (kb == NULL) {
+		_E("out of memory");
+		return AUL_R_ERROR;
+	}
+
+	bundle_add_str(kb, AUL_K_WIDGET_ID, widget_id);
+	bundle_add_str(kb, AUL_K_WIDGET_INSTANCE_ID, instance_id);
+
+	ret = aul_sock_send_bundle(AUL_UTIL_PID, getuid(), WIDGET_ADD, kb,
+		AUL_SOCK_NONE);
+
+	bundle_free(kb);
+
+	return aul_error_convert(ret);
+}
+
+API int aul_widget_instance_del(const char *widget_id, const char *instance_id)
+{
+	int ret;
+	bundle *kb;
+
+	if (widget_id == NULL || instance_id == NULL)
+		return AUL_R_EINVAL;
+
+	kb = bundle_create();
+	if (kb == NULL) {
+		_E("out of memory");
+		return AUL_R_ERROR;
+	}
+
+	bundle_add_str(kb, AUL_K_WIDGET_ID, widget_id);
+	bundle_add_str(kb, AUL_K_WIDGET_INSTANCE_ID, instance_id);
+
+	ret = aul_sock_send_bundle(AUL_UTIL_PID, getuid(), WIDGET_DEL, kb,
+		AUL_SOCK_NONE);
+
+	bundle_free(kb);
+
+	return aul_error_convert(ret);
+}
+
+struct __cb_data {
+	aul_widget_instance_foreach_cb cb;
+	void *data;
+};
+
+static void __foreach_cb(const char *key, const int type, const bundle_keyval_t *kv, void *user_data)
+{
+	struct __cb_data *cb_data = (struct __cb_data *)user_data;
+
+	cb_data->cb(key, cb_data->data);
+}
+
+API int aul_widget_instance_foreach(const char *widget_id, aul_widget_instance_foreach_cb cb, void *data)
+{
+	int ret;
+	int fd;
+	bundle *kb;
+	app_pkt_t *pkt = NULL;
+	bundle *list_kb = NULL;
+	struct __cb_data cb_data;
+
+	if (widget_id == NULL)
+		return AUL_R_EINVAL;
+
+	kb = bundle_create();
+	if (kb == NULL) {
+		_E("out of memory");
+		return AUL_R_ERROR;
+	}
+
+	bundle_add_str(kb, AUL_K_APPID, __to_appid(widget_id));
+	bundle_add_str(kb, AUL_K_WIDGET_ID, widget_id);
+
+	fd = aul_sock_send_bundle(AUL_UTIL_PID, getuid(), WIDGET_LIST, kb,
+		AUL_SOCK_ASYNC);
+
+	if (fd > 0) {
+		ret = aul_sock_recv_reply_pkt(fd, &pkt);
+		if (ret < 0 || pkt == NULL) {
+			_E("failed to get instance list of %s", widget_id);
+		} else {
+			list_kb = bundle_decode(pkt->data, pkt->len);
+			if (list_kb) {
+				cb_data.cb = cb;
+				cb_data.data = data;
+				bundle_foreach(list_kb, __foreach_cb, &cb_data);
+			}
+		}
+	} else {
+		ret = fd;
+	}
+
+	if (pkt)
+		free(pkt);
+
+	bundle_free(kb);
+
+	return aul_error_convert(ret);
+}
+
+extern int app_request_to_launchpad_for_uid(int cmd, const char *appid, bundle *kb, uid_t uid);
+
+API int aul_widget_instance_update(const char *widget_id, const char *instance_id, bundle *param)
+{
+	int ret;
+	bundle *kb = param;
+	const char *appid;
+
+	if (widget_id == NULL || instance_id == NULL)
+		return AUL_R_EINVAL;
+
+	if (kb == NULL)
+		kb = bundle_create();
+
+	if (kb == NULL) {
+		_E("out of memory");
+		return AUL_R_ERROR;
+	}
+
+	appid = __to_appid(widget_id);
+
+	bundle_add_str(kb, AUL_K_WIDGET_ID, widget_id);
+	bundle_add_str(kb, AUL_K_WIDGET_INSTANCE_ID, instance_id);
+
+	ret = app_request_to_launchpad_for_uid(WIDGET_UPDATE, appid, kb, getuid());
+
+	if (param == NULL)
+		bundle_free(kb);
+
+	return ret;
+}
+
